d3adf77c59b22e80b7347ff348009a7e5bd96359
lagun123420 <lagun123420@gmail.com>
2022-10-26 13:47:27 +0300

    Add rules for github pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Works from './pages/Works/Works';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home/>}></Route>
           <Route path="/works" element={<Works/>}></Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
